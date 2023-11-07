a310f318059274703210d1fb77706baafdf9ec82
Felipe Augusto Machado Sampaio Miguel <felipeasmiguel@gmail.com>
2023-11-07 11:34:06 -0300

    Questão 13 corrigida

--- lista51/questao13.py
+++ lista51/questao13.py
@@ -3,15 +3,12 @@ Desenvolver um programa que imprima a tabuada de 3 a 6.
 '''
 
 # Processamento e Output
-# Tabuada de 3
-cont1 = 1
-print("Tabuada de 3:")
-while cont1 <= 10:
-    print(f"3x{cont1}={3*cont1}")
-    cont1 = cont1 + 1
-# Tabuada de 6
-cont2 = 1
-print("Tabuada de 6:")
-while cont2 <= 10:
-    print(f"6x{cont2}={6*cont2}")
-    cont2 = cont2 + 1
+fator1 = 3
+fator2 = 1
+
+while fator1 <= 6:
+    while fator2<= 10:
+        print(f"{fator1}x{fator2}={fator2*fator1}")
+        fator2 = fator2 + 1
+    fator1 = fator1 + 1
+    fator2 = 1
